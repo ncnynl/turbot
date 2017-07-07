2bea4a42a0550a36778aaa2026ebd06ee1957240
ncnynl <1043931@qq.com>
2017-07-07 22:59:47 +0800

    add A1 laser

---
+++ turbot_code/scripts/go_to_specific_point_on_map.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+# TurtleBot must have minimal.launch & amcl_demo.launch
+# running prior to starting this script
+# For simulation: launch gazebo world & amcl_demo prior to run this script
+
+import rospy
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose, Point, Quaternion
+import sys
+
+class GoToPose():
+    def __init__(self):
+
+        self.goal_sent = False
+
+	# What to do if shut down (e.g. Ctrl-C or failure)
+	rospy.on_shutdown(self.shutdown)
+	
+	# Tell the action client that we want to spin a thread by default
+	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+	rospy.loginfo("Wait for the action server to come up")
+
+	# Allow up to 5 seconds for the action server to come up
+	self.move_base.wait_for_server(rospy.Duration(5))
+
+    def goto(self, pos, quat):
+
+        # Send a goal
+        self.goal_sent = True
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = 'map'
+	goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+
+	# Start moving
+        self.move_base.send_goal(goal)
+
+	# Allow TurtleBot up to 60 seconds to complete task
+	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+
+        state = self.move_base.get_state()
+        result = False
+
+        if success and state == GoalStatus.SUCCEEDED:
+            # We made it!
+            result = True
+        else:
+            self.move_base.cancel_goal()
+
+        self.goal_sent = False
+        return result
+
+    def shutdown(self):
+        if self.goal_sent:
+            self.move_base.cancel_goal()
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+
+if __name__ == '__main__':
+    try:
+
+	if len(sys.argv)!=3:
+	    sys.exit("Format is : rosrun turbot_code go_to_specific_point_on_map.py x y ")
+
+        rospy.init_node('nav_test', anonymous=False)
+        navigator = GoToPose()
+	if sys.argv[1]:
+	    px = sys.argv[1]
+	    rospy.loginfo("The position x is %s", px)
+	else:        
+            sys.exit("Please input x")
+
+	if sys.argv[2]:
+           py = sys.argv[2]
+	   rospy.loginfo("The position y is %s", py)     
+	else:
+           sys.exit("Please input y")
+
+        # Customize the following values so they are appropriate for your location
+        position = {'x': px, 'y' : py}
+        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
+
+        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+        success = navigator.goto(position, quaternion)
+
+        if success:
+            rospy.loginfo("Hooray, reached the desired pose")
+        else:
+            rospy.loginfo("The base failed to reach the desired pose")
+
+        # Sleep to give the last log messages time to be sent
+        rospy.sleep(1)
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Ctrl-C caught. Quitting")

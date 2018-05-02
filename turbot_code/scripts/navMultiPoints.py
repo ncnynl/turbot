0931e02af3e3bea237e87c7920f9c41b9aaf2c18
ncnynl <ncnynl@gmail.com>
2018-05-02 12:41:25 +0800

    add hokuyo-utm-30lx

--- turbot_code/scripts/navMultiPoints.py
+++ turbot_code/scripts/navMultiPoints.py
@@ -32,12 +32,12 @@ class NavTest():
         # that was used to launch RViz.
         locations = dict()
         
-        locations['hall_foyer'] = Pose(Point(0.643, 4.720, 0.000), Quaternion(0.000, 0.000, 0.223, 0.975))
-        locations['hall_kitchen'] = Pose(Point(-1.994, 4.382, 0.000), Quaternion(0.000, 0.000, -0.670, 0.743))
-        locations['hall_bedroom'] = Pose(Point(-3.719, 4.401, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
-        locations['living_room_1'] = Pose(Point(0.720, 2.229, 0.000), Quaternion(0.000, 0.000, 0.786, 0.618))
-        locations['living_room_2'] = Pose(Point(1.471, 1.007, 0.000), Quaternion(0.000, 0.000, 0.480, 0.877))
-        locations['dining_room_1'] = Pose(Point(-0.861, -0.019, 0.000), Quaternion(0.000, 0.000, 0.892, -0.451))
+        locations['hall_foyer'] = Pose(Point(-0.109, 1.25, -00534), Quaternion(0.000, 0.000, 0.223, 0.975))
+        locations['hall_kitchen'] = Pose(Point(0.796, 0.856, 0.000), Quaternion(0.000, 0.000, -0.670, 0.743))
+        locations['hall_bedroom'] = Pose(Point(0.397, -0.0962, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
+        locations['living_room_1'] = Pose(Point(-0.278, -0.214, 0.000), Quaternion(0.000, 0.000, 0.786, 0.618))
+        locations['living_room_2'] = Pose(Point(0.0766, 0.132, 0.000), Quaternion(0.000, 0.000, 0.480, 0.877))
+        locations['dining_room_1'] = Pose(Point(0.375, 0.407, 0.000), Quaternion(0.000, 0.000, 0.892, -0.451))
         
         # Publisher to manually control the robot (e.g. to stop it, queue_size=5)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5)
